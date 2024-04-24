8b84edfe4f868bc180cad484ac3ae75c5e6eef96
MaSeongJin <making1104@naver.com>
2024-04-24 14:35:44 +0900

    :zap: Update:repository class 이름 수정

--- BE/user-service/src/main/java/com/mealtoyou/userservice/domain/repository/UserRepository.java
+++ BE/user-service/src/main/java/com/mealtoyou/userservice/domain/repository/UserRepository.java
@@ -3,5 +3,5 @@ package com.mealtoyou.userservice.domain.repository;
 import com.mealtoyou.userservice.domain.model.User;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
-public interface userRepository extends ReactiveCrudRepository<User, Long> {
+public interface UserRepository extends ReactiveCrudRepository<User, Long> {
 }
