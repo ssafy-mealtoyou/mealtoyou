810b2822ce23c7a4c8a88ec96058e53418b796f2
MaSeongJin <making1104@naver.com>
2024-05-02 16:59:32 +0900

    :sparkles: Feat:chatting-service 인증 관련 로직 추가

---
+++ BE/chatting-service/src/main/java/com/mealtoyou/chattingservice/application/dto/RefreshToken.java
@@ -0,0 +1,22 @@
+package com.mealtoyou.chattingservice.application.dto;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+
+@Getter
+@RedisHash(value = "refreshToken")
+public class RefreshToken {
+
+    @Id
+    private String refreshToken;
+    @TimeToLive
+    @Value("${REFRESH_TOKEN_EXPIRE_TIME}")
+    private Long expireTime;
+
+    public RefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+}
