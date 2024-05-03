ed1f08cf7c34644177d3c2b87e90b5efa2e4a79a
MaSeongJin <making1104@naver.com>
2024-05-03 14:24:19 +0900

    :sparkles: Feat:websocket 설정 및 서비스 구현

--- BE/chatting-service/src/main/java/com/mealtoyou/chattingservice/application/service/RouteService.java
+++ BE/chatting-service/src/main/java/com/mealtoyou/chattingservice/application/service/RouteService.java
@@ -1,15 +1,14 @@
 package com.mealtoyou.chattingservice.application.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mealtoyou.chattingservice.domain.model.Chat;
 import com.mealtoyou.chattingservice.domain.repository.ChatRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
-
-import java.time.LocalDateTime;
 
 @Slf4j
 @Service
@@ -17,23 +16,27 @@ import java.time.LocalDateTime;
 public class RouteService {
 
     private final KafkaTemplate<Object, String> kafkaTemplate;
-    private final SimpMessageSendingOperations messageSendingOperations;
     private final ChatRepository chatRepository;
+    private final ObjectMapper objectMapper;
 
-    public Mono<String> route(String message, Long userId, Long groupId) {
-        log.info("Received message: {}", message);
-
-        Chat chat = new Chat();
-        chat.setUserId(userId);
-        chat.setGroupId(groupId);
-        chat.setMessage(message);
-        chat.setTimestamp(LocalDateTime.now());
-
-        // Save the chat message to MongoDB
+    public Mono<Chat> route(Chat chat) {
+        // Save the chat message to MongoDB and send it to Kafka
         return chatRepository.save(chat)
-                .flatMap(savedChat -> Mono.fromRunnable(() -> {
-                    kafkaTemplate.send("group-chat-topic", message);
-//                    messageSendingOperations.convertAndSend("/group-chat", message);
-                }).thenReturn(message));
+                .flatMap(savedChat -> {
+                    // Send the chat message to Kafka as JSON
+                    String jsonChat = convertChatToJson(savedChat);
+                    kafkaTemplate.send("group-chat-topic", jsonChat);
+                    // Finish by returning the saved chat
+                    return Mono.just(savedChat);
+                });
+    }
+
+    private String convertChatToJson(Chat chat) {
+        try {
+            return objectMapper.writeValueAsString(chat);
+        } catch (JsonProcessingException e) {
+            log.error("Error converting chat to JSON: {}", e.getMessage());
+            throw new RuntimeException("Error converting chat to JSON");
+        }
     }
 }
--- BE/chatting-service/src/main/java/com/mealtoyou/chattingservice/infrastructure/handler/ChatWebSocketHandler.java
+++ BE/chatting-service/src/main/java/com/mealtoyou/chattingservice/infrastructure/handler/ChatWebSocketHandler.java
@@ -1,7 +1,11 @@
 package com.mealtoyou.chattingservice.infrastructure.handler;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mealtoyou.chattingservice.application.service.ChatCacheService;
 import com.mealtoyou.chattingservice.application.service.JwtTokenProvider;
 import com.mealtoyou.chattingservice.application.service.RouteService;
+import com.mealtoyou.chattingservice.domain.model.Chat;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.lang.NonNull;
@@ -13,6 +17,8 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.publisher.SignalType;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -23,9 +29,11 @@ import java.util.concurrent.CopyOnWriteArrayList;
 @Component
 @RequiredArgsConstructor
 public class ChatWebSocketHandler implements WebSocketHandler {
-    private final RouteService routeService; // 메시지 라우팅을 처리하는 서비스
-    private final Map<Long, List<WebSocketSession>> groupSessions = new ConcurrentHashMap<>(); // 각 그룹에 대한 WebSocket 세션 목록을 관리하는 맵
+    private final RouteService routeService;
+    private final Map<Long, List<WebSocketSession>> groupSessions = new ConcurrentHashMap<>();
     private final JwtTokenProvider jwtTokenProvider;
+    private final ChatCacheService chatCacheService;
+    private final ObjectMapper objectMapper;
 
     @Override
     @NonNull
@@ -38,53 +46,95 @@ public class ChatWebSocketHandler implements WebSocketHandler {
 
         groupSessions.computeIfAbsent(groupId, k -> new CopyOnWriteArrayList<>()).add(session);
 
-        return session
-                .receive()
-                .flatMap(message -> handleMessage(session, message, groupId, session.getId()))
+        // Send previous chats first
+        Mono<Void> sendPreviousChats = sendPreviousChatsToUser(groupId, session);
+
+        return sendPreviousChats
+                .thenMany(session.receive()
+                        .flatMap(message -> handleMessage(session, message, groupId, session.getId()))
+                        .doFinally(signalType -> {
+                            if (signalType == SignalType.CANCEL) {
+                                log.info("Client terminated the connection");
+                                removeSessionFromGroup(groupId, session);
+                            }
+                        }))
                 .then(Mono.fromRunnable(() -> removeSessionFromGroup(groupId, session)))
                 .onErrorResume(throwable -> {
                     log.warn("Error during WebSocket handling: {}", throwable.getMessage());
                     return Mono.empty();
-                })
-                .doFinally(signalType -> {
-                    if (signalType == SignalType.CANCEL) {
-                        // Handle user-initiated disconnection
-                        log.info("Client terminated the connection");
-                        removeSessionFromGroup(groupId, session);
-                    }
                 }).then();
     }
 
+    public Mono<Void> sendPreviousChatsToUser(Long groupId, WebSocketSession session) {
+        log.info("채팅 조회");
+        return chatCacheService.getRecentChatsFromMongo(groupId, 20)
+                .doOnNext(chats -> log.info("채팅 조회 결과: {}", chats)) // Log the fetched chats
+                .flatMapMany(Flux::fromIterable)
+                .flatMap(chat -> session.send(Mono.just(session.textMessage(chat.getMessage()))))
+                .then();
+    }
 
-    // 클라이언트로부터 수신된 메시지 처리
     private Mono<Void> handleMessage(WebSocketSession session, WebSocketMessage message, Long groupId, String senderSessionId) {
         log.info("클라이언트로부터 메시지 수신: {}", message.getPayloadAsText());
 
-        // 클라이언트가 "CLOSE_MESSAGE"를 보내면 연결 종료
         if (message.getPayloadAsText().equals("CLOSE_MESSAGE")) {
             return session.close();
         }
 
-        // userId 가져와서 WebSocket 세션에 저장
-        Long userId = getUserIdFromSession(session); // 여기에서 userId 가져오는 메소드를 호출하세요.
+        Long userId = getUserIdFromSession(session);
 
-        // 그룹 내에서 메시지 라우팅
-        return routeService.route(message.getPayloadAsText(), userId, groupId)
-                .flatMap(savedMessage -> broadcastMessageToGroup(groupId, savedMessage, senderSessionId)) // 그룹 내 모든 클라이언트에게 메시지 전송
+        Chat chat;
+        try {
+            chat = objectMapper.readValue(message.getPayloadAsText(), Chat.class);
+        } catch (IOException e) {
+            log.error("Error deserializing Chat object: {}", e.getMessage());
+            return Mono.error(e);
+        }
+        chat.setUserId(userId);
+        chat.setGroupId(groupId);
+        chat.setTimestamp(LocalDateTime.now());
+
+        return routeService.route(chat)
+                .flatMap(savedMessage -> broadcastMessageToGroup(chat, senderSessionId))
+                .then()
+                .onErrorResume(throwable -> {
+                    log.warn("Error during message handling: {}", throwable.getMessage());
+                    return Mono.empty();
+                });
+
+    }
+
+    private Mono<Void> broadcastMessageToGroup(Chat chat, String senderSessionId) {
+        List<WebSocketSession> sessions = groupSessions.getOrDefault(chat.getGroupId(), Collections.emptyList());
+
+        return Flux.fromIterable(sessions)
+                .filter(session -> !session.getId().equals(senderSessionId))
+                .flatMap(session -> {
+                    if (session.isOpen()) {
+                        String jsonMessage = convertChatToJson(chat);
+                        return session.send(Mono.just(session.textMessage(jsonMessage)))
+                                .then()
+                                .doOnSuccess(success -> log.info("Message sent successfully to session {}", session.getId()))
+                                .doOnError(error -> log.error("Error sending message to session {}: {}", session.getId(), error.getMessage()))
+                                .onErrorResume(error -> Mono.empty()); // Continue with empty Mono on error
+                    } else {
+                        log.warn("Session {} is closed. Skipping message send operation.", session.getId());
+                        return Mono.empty();
+                    }
+                })
                 .then();
     }
 
-    // 그룹 내 모든 클라이언트에게 메시지 전송
-    private Mono<Void> broadcastMessageToGroup(Long groupId, String message, String senderSessionId) {
-        List<WebSocketSession> sessions = groupSessions.getOrDefault(groupId, Collections.emptyList());
 
-        return Flux.fromIterable(sessions)
-                .filter(s -> !s.getId().equals(senderSessionId)) // 보낸 클라이언트를 제외하고
-                .flatMap(s -> s.send(Mono.just(s.textMessage(message)))) // 메시지 전송
-                .then(); // Flux<Void>를 Mono<Void>로 변환
+    private String convertChatToJson(Chat chat) {
+        try {
+            return objectMapper.writeValueAsString(chat);
+        } catch (JsonProcessingException e) {
+            log.error("Error converting chat to JSON: {}", e.getMessage());
+            throw new RuntimeException("Error converting chat to JSON");
+        }
     }
 
-    // 그룹에서 세션 제거
     private void removeSessionFromGroup(Long groupId, WebSocketSession session) {
         List<WebSocketSession> sessions = groupSessions.get(groupId);
         if (sessions != null) {
@@ -95,7 +145,6 @@ public class ChatWebSocketHandler implements WebSocketHandler {
         }
     }
 
-    // 웹소켓 주소에서 그룹 정보 추출
     private Long parseGroupFromWebSocketAddress(String webSocketAddress) {
         if (webSocketAddress.contains("/chat/group:")) {
             int startIndex = webSocketAddress.indexOf("/chat/group:") + "/chat/group:".length();
@@ -110,14 +159,10 @@ public class ChatWebSocketHandler implements WebSocketHandler {
     }
 
     private Long getUserIdFromSession(WebSocketSession session) {
-        // 여기에서 userId를 가져오는 로직을 구현하세요.
-        // 예를 들어, 세션의 특정 헤더에서 userId를 가져오거나, 인증된 사용자 객체에서 userId를 추출할 수 있습니다.
-        // 이 예제에서는 헤더에서 userId를 가져오는 것으로 가정합니다.
         String token = session.getHandshakeInfo().getHeaders().getFirst("token");
-        if(token == null) {
+        if (token == null) {
             throw new RuntimeException("token 조회 실패");
         }
         return jwtTokenProvider.getUserId(token);
     }
-
 }
