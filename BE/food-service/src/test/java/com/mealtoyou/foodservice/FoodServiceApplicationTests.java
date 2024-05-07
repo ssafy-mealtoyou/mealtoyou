953ee793ce69641ee61dbae31f80552de83cb297
최지희 <cjh0313c@naver.com>
2024-05-07 12:19:20 +0900

    :recycle: Refactor: reformat code indent

--- BE/food-service/src/test/java/com/mealtoyou/foodservice/FoodServiceApplicationTests.java
+++ BE/food-service/src/test/java/com/mealtoyou/foodservice/FoodServiceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FoodServiceApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
