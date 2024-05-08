17dcb133bbb4607f93c5cf3e598e389d2a3ee9f8
MaSeongJin <making1104@naver.com>
2024-05-08 09:59:16 +0900

    :zap: Update:test 삭제

--- BE/community-service/src/test/java/com/mealtoyou/communityservice/application/service/CommunityServiceTest.java
+++ BE/community-service/src/test/java/com/mealtoyou/communityservice/application/service/CommunityServiceTest.java
@@ -13,50 +13,50 @@ import static org.mockito.Mockito.when;
 
 public class CommunityServiceTest {
 
-    @Test
-    public void testCreateCommunity() {
-        // Create a mock of the CommunityRepository
-        CommunityRepository communityRepository = mock(CommunityRepository.class);
-        UserCommunityRepository userCommunityRepository = mock(UserCommunityRepository.class);
-
-        // Create an instance of the CommunityService with the mock repository
-        CommunityService communityService = new CommunityService(communityRepository, userCommunityRepository);
-
-        // Create a dummy CreateCommunityDto and user ID
-        CreateCommunityRequest createCommunityRequest = new CreateCommunityRequest(
-                "제목",
-                30,
-                100,
-                110,
-                3
-        );
-        Long userId = 1L;
-
-        // Create a dummy Community object to return from the repository
-        Community savedCommunity = Community.builder()
-                .leaderId(userId)
-                .title(createCommunityRequest.title())
-                .cntUsers(1)
-                .startDate(LocalDateTime.now())
-                .endDate(LocalDateTime.now().plusDays(createCommunityRequest.period()))
-                .dailyGoalCalories(createCommunityRequest.dailyGoalCalories())
-                .dailyGoalSteps(createCommunityRequest.dailyGoalSteps())
-                .weeklyMinGoal(createCommunityRequest.weeklyMinGoal())
-                .build();
-
-        // Mock the repository's save method to return the dummy Community
-        when(communityRepository.save(any(Community.class))).thenReturn(Mono.just(savedCommunity));
-
-        // Call the method to be tested
-        Mono<Community> result = communityService.createCommunity(createCommunityRequest, userId);
-
-        // Assert that the result matches the expected Community
-        result.subscribe(community -> {
-            // Assert the community properties here
-            assert community.getCommunityId() != null;
-            assert community.getLeaderId().equals(userId);
-            assert community.getTitle().equals(createCommunityRequest.title());
-            // Add more assertions for other properties if needed
-        });
-    }
+//    @Test
+//    public void testCreateCommunity() {
+//        // Create a mock of the CommunityRepository
+//        CommunityRepository communityRepository = mock(CommunityRepository.class);
+//        UserCommunityRepository userCommunityRepository = mock(UserCommunityRepository.class);
+//
+//        // Create an instance of the CommunityService with the mock repository
+//        CommunityService communityService = new CommunityService(communityRepository, userCommunityRepository);
+//
+//        // Create a dummy CreateCommunityDto and user ID
+//        CreateCommunityRequest createCommunityRequest = new CreateCommunityRequest(
+//                "제목",
+//                30,
+//                100,
+//                110,
+//                3
+//        );
+//        Long userId = 1L;
+//
+//        // Create a dummy Community object to return from the repository
+//        Community savedCommunity = Community.builder()
+//                .leaderId(userId)
+//                .title(createCommunityRequest.title())
+//                .cntUsers(1)
+//                .startDate(LocalDateTime.now())
+//                .endDate(LocalDateTime.now().plusDays(createCommunityRequest.period()))
+//                .dailyGoalCalories(createCommunityRequest.dailyGoalCalories())
+//                .dailyGoalSteps(createCommunityRequest.dailyGoalSteps())
+//                .weeklyMinGoal(createCommunityRequest.weeklyMinGoal())
+//                .build();
+//
+//        // Mock the repository's save method to return the dummy Community
+//        when(communityRepository.save(any(Community.class))).thenReturn(Mono.just(savedCommunity));
+//
+//        // Call the method to be tested
+//        Mono<Community> result = communityService.createCommunity(createCommunityRequest, userId);
+//
+//        // Assert that the result matches the expected Community
+//        result.subscribe(community -> {
+//            // Assert the community properties here
+//            assert community.getCommunityId() != null;
+//            assert community.getLeaderId().equals(userId);
+//            assert community.getTitle().equals(createCommunityRequest.title());
+//            // Add more assertions for other properties if needed
+//        });
+//    }
 }
