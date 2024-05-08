23a8193724c99223197fddec8e3f301975e01d46
최지희 <cjh0313c@naver.com>
2024-05-08 13:51:44 +0900

    :sparkles: Feat: 알림

--- FE/app/src/main/java/com/example/mealtoyou/MainActivity.kt
+++ FE/app/src/main/java/com/example/mealtoyou/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.mealtoyou
 
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.animation.AnimatedContentTransitionScope
 import androidx.compose.animation.core.tween
 import androidx.compose.material3.Surface
@@ -19,6 +22,7 @@ import com.example.mealtoyou.ui.theme.MealToYouTheme
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.getValue
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import com.example.mealtoyou.ui.theme.diet.DietPage
@@ -28,6 +32,7 @@ import com.example.mealtoyou.ui.theme.login.LoginPage
 import com.example.mealtoyou.ui.theme.main.MainPage
 import com.example.mealtoyou.ui.theme.report.ReportPage
 import com.example.mealtoyou.ui.theme.shared.BottomNavigationBar
+import android.Manifest.permission.POST_NOTIFICATIONS
 
 class MainActivity : ComponentActivity() {
 
@@ -93,6 +98,35 @@ class MainActivity : ComponentActivity() {
                         MyPage()
                     }
                 }
+            }
+        }
+    }
+    // Declare the launcher at the top of your Activity/Fragment:
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission(),
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            // FCM SDK (and your app) can post notifications.
+        } else {
+            // TODO: Inform user that that your app will not show notifications.
+        }
+    }
+
+    private fun askNotificationPermission() {
+        // This is only necessary for API level >= 33 (TIRAMISU)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, POST_NOTIFICATIONS) ==
+                PackageManager.PERMISSION_GRANTED
+            ) {
+                // FCM SDK (and your app) can post notifications.
+            } else if (shouldShowRequestPermissionRationale(POST_NOTIFICATIONS)) {
+                // TODO: display an educational UI explaining to the user the features that will be enabled
+                //       by them granting the POST_NOTIFICATION permission. This UI should provide the user
+                //       "OK" and "No thanks" buttons. If the user selects "OK," directly request the permission.
+                //       If the user selects "No thanks," allow the user to continue without notifications.
+            } else {
+                // Directly ask for the permission
+                requestPermissionLauncher.launch(POST_NOTIFICATIONS)
             }
         }
     }
