2f1652ada090a8a8331475f9075afe0283bce110
MaSeongJin <making1104@naver.com>
2024-05-09 16:42:50 +0900

    :sparkles: Feat:kafka user 정보 전달 로직 추가

--- BE/user-service/src/main/java/com/mealtoyou/userservice/domain/repository/UserRepository.java
+++ BE/user-service/src/main/java/com/mealtoyou/userservice/domain/repository/UserRepository.java
@@ -2,12 +2,17 @@ package com.mealtoyou.userservice.domain.repository;
 
 import com.mealtoyou.userservice.domain.model.User;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Repository
 public interface UserRepository extends ReactiveCrudRepository<User, Long> {
 
     Mono<User> findByEmail(String email);
+    @NonNull
+    Flux<User> findAllById(@NonNull Iterable<Long> userIds);
+
 
 }
