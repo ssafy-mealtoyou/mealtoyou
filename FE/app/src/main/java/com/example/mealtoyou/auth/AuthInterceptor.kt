75608eec75276ac4053cf76091bd5eee8f9b8c5e
최지희 <cjh0313c@naver.com>
2024-05-14 12:34:16 +0900

    :zap: Update: 로그인 매번 인터셉터에서 토큰 가져오도록 수정

--- FE/app/src/main/java/com/example/mealtoyou/auth/AuthInterceptor.kt
+++ FE/app/src/main/java/com/example/mealtoyou/auth/AuthInterceptor.kt
@@ -1,14 +1,16 @@
 package com.example.mealtoyou.auth
 
 import android.util.Log
+import com.example.mealtoyou.MainApplication
 import okhttp3.Interceptor
 import okhttp3.Response
 
 class AuthInterceptor(private val token: String) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val originalRequest = chain.request()
+        val accessToken= MainApplication.prefs.getValue("accessToken")
         val newRequest = originalRequest.newBuilder()
-            .header("Authorization", "Bearer $token")
+            .header("Authorization", "Bearer $accessToken")
             .build()
         Log.e("auth","${newRequest.header("Authorization")}")
         return chain.proceed(newRequest)
