5e8316b56a6dcc7eb5c2bb86b62de662afe0f7a4
김지민 <mnvyiions@gmail.com>
2024-05-18 05:55:28 +0900

    :bug: Fix: 식단 등록 API null 값 예외 처리

--- BE/food-service/src/main/java/com/mealtoyou/foodservice/application/service/DietService.java
+++ BE/food-service/src/main/java/com/mealtoyou/foodservice/application/service/DietService.java
@@ -6,10 +6,10 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.mealtoyou.foodservice.application.dto.CommunityDietDto;
@@ -30,7 +30,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import reactor.util.function.Tuples;
+import reactor.util.function.Tuple3;
 
 @Service
 @RequiredArgsConstructor
@@ -54,59 +54,81 @@ public class DietService {
         return kafkaMonoUtils.sendAndReceive("user-service-getNickname", userId);
     }
 
-    @Transactional
     public Mono<Void> createDiet(long userId, List<DietFoodRequestDto> dietFoodRequestDtoList) {
         return Flux.fromIterable(dietFoodRequestDtoList)
-                .flatMap(dto -> foodRepository.findByRid(dto.foodId()))
-                .collectList()
-                .flatMap(foods -> {
-                    if (foods.size() != dietFoodRequestDtoList.size()) {
-                        return Mono.error(new ResponseStatusException(HttpStatus.BAD_REQUEST));
-                    }
+            .flatMap(dto -> foodRepository.findByRid(dto.foodId())
+                .switchIfEmpty(Mono.error(
+                    new ResponseStatusException(HttpStatus.BAD_REQUEST, "음식 ID가 유효하지 않습니다: " + dto.foodId()))))
+            .collectList()
+            .flatMap(foods -> {
+                if (foods.size() != dietFoodRequestDtoList.size()) {
+                    return Mono.error(
+                        new ResponseStatusException(HttpStatus.BAD_REQUEST, "요청된 음식의 수량과 일치하지 않는 음식이 있습니다."));
+                }
 
-                    Diet diet = Diet.builder()
-                            .userId(userId)
-                            .createDateTime(LocalDateTime.now())
-                            .build();
+                Diet diet = createDiet(userId);
+                return saveDietWithFoods(diet, foods, dietFoodRequestDtoList);
+            })
+            .flatMapMany(this::saveDietFoods)
+            .then();
+    }
 
-                    return dietRepository.save(diet).map(savedDiet -> Tuples.of(savedDiet, foods));
-                })
-                .flatMap(tuple -> {
-                    Diet savedDiet = tuple.getT1();
-                    List<Food> foods = tuple.getT2();
+    private Diet createDiet(long userId) {
+        return Diet.builder()
+            .userId(userId)
+            .createDateTime(LocalDateTime.now())
+            .build();
+    }
 
-                    double totalCalories = 0.0;
-                    double totalCarbohydrate = 0.0;
-                    double totalProtein = 0.0;
-                    double totalFat = 0.0;
+    private Mono<Tuple3<Diet, List<Food>, List<Double>>> saveDietWithFoods(
+        Diet diet, List<Food> foods,
+        List<DietFoodRequestDto> dietFoodRequestDtoList
+    ) {
+        return dietRepository.save(diet)
+            .map(savedDiet -> calculateTotalNutrients(savedDiet, foods, dietFoodRequestDtoList))
+            .flatMap(savedDiet -> {
+                Mono<Diet> savedDietMono = dietRepository.save(savedDiet);
+                Mono<List<Food>> foodListMono = Mono.just(foods);
+                Mono<List<Double>> servingSizeListMono = Mono.just(
+                    dietFoodRequestDtoList.stream().map(DietFoodRequestDto::servingSize).toList());
+                return Mono.zip(savedDietMono, foodListMono, servingSizeListMono);
+            });
+    }
 
-                    for (int i = 0; i < dietFoodRequestDtoList.size(); i++) {
-                        double factor = dietFoodRequestDtoList.get(i).servingSize();
-                        totalCalories += foods.get(i).getEnergy() * factor;
-                        totalCarbohydrate += foods.get(i).getCarbohydrate() * factor;
-                        totalProtein += foods.get(i).getProtein() * factor;
-                        totalFat += foods.get(i).getFat() * factor;
-                    }
+    private Diet calculateTotalNutrients(Diet diet, List<Food> foods, List<DietFoodRequestDto> dietFoodRequestDtoList) {
+        double totalCalories = 0.0;
+        double totalCarbohydrate = 0.0;
+        double totalProtein = 0.0;
+        double totalFat = 0.0;
 
-                    savedDiet.updateTotalNutrients(totalCalories, totalCarbohydrate, totalProtein, totalFat);
-                    return dietRepository.save(savedDiet).zipWith(Mono.just(foods));
-                })
-                .flatMapMany(tuple -> {
-                    List<DietFood> dietFoods = new ArrayList<>();
-                    Diet savedDiet = tuple.getT1();
-                    List<Food> foods = tuple.getT2();
-                    for (int i = 0; i < dietFoodRequestDtoList.size(); i++) {
-                        Food food = foods.get(i);
-                        double factor = dietFoodRequestDtoList.get(i).servingSize();
-                        dietFoods.add(DietFood.builder()
-                                .dietId(savedDiet.getDietId())
-                                .foodId(food.getRid())
-                                .servingSize(factor)
-                                .build());
-                    }
-                    return dietFoodRepository.saveAll(dietFoods);
-                })
-                .then();
+        for (int i = 0; i < dietFoodRequestDtoList.size(); i++) {
+            Food food = foods.get(i);
+            double factor = dietFoodRequestDtoList.get(i).servingSize();
+            totalCalories += Optional.ofNullable(food.getEnergy()).orElse(0.0) * factor;
+            totalCarbohydrate += Optional.ofNullable(food.getCarbohydrate()).orElse(0.0) * factor;
+            totalProtein += Optional.ofNullable(food.getProtein()).orElse(0.0) * factor;
+            totalFat += Optional.ofNullable(food.getFat()).orElse(0.0) * factor;
+        }
+
+        diet.updateTotalNutrients(totalCalories, totalCarbohydrate, totalProtein, totalFat);
+        return diet;
+    }
+
+    private Flux<DietFood> saveDietFoods(Tuple3<Diet, List<Food>, List<Double>> tuple3) {
+        List<DietFood> dietFoods = new ArrayList<>();
+        Diet savedDiet = tuple3.getT1();
+        List<Food> foods = tuple3.getT2();
+        List<Double> servingSizeList = tuple3.getT3();
+        for (int i = 0; i < foods.size(); i++) {
+            Food food = foods.get(i);
+            double servingSize = servingSizeList.get(i);
+            dietFoods.add(DietFood.builder()
+                .dietId(savedDiet.getDietId())
+                .foodId(food.getRid())
+                .servingSize(servingSize)
+                .build());
+        }
+        return dietFoodRepository.saveAll(dietFoods);
     }
 
     public Mono<DailyDietsResponseDto> getMyDiet(long userId, String dateString) {
