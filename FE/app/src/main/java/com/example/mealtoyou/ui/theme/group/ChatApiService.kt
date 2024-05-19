64a6512ee5181b73c54651fad4b7b8abed4e2989
SSAFY <jmg97@kakao.com>
2024-05-19 19:57:09 +0900

    hotfix : 날짜 고정값 버그 수정

--- FE/app/src/main/java/com/example/mealtoyou/ui/theme/group/ChatApiService.kt
+++ FE/app/src/main/java/com/example/mealtoyou/ui/theme/group/ChatApiService.kt
@@ -6,13 +6,27 @@ import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Path
+import retrofit2.http.Query
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 interface ChatApiService {
     @GET("api/community-service/communities/users/{userId}")
     suspend fun getUserImage(@Path("userId") userId: Int): Response<List<UserSimpleData>>
 
 
-    @GET("api/food-service/api/diets?date=2024-05-10")
-    suspend fun getUserDiets(@Header("Authorization") authorization: String): Response<DailyDietsResponseDto>
+    @GET("api/food-service/api/diets")
+    suspend fun getUserDiets(
+        @Header("Authorization") authorization: String,
+        @Query("date") date: String = getCurrentDate()
+    ): Response<DailyDietsResponseDto>
+
+    companion object {
+        fun getCurrentDate(): String {
+            val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+            return dateFormat.format(Date())
+        }
+    }
 }
 
